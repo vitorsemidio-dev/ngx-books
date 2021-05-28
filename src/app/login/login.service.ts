de14d75d8a134ec1ba3080f19acfabcd871ba646
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-05-28 07:23:37 -0300

    Implementação fazerLogin em LoginService

--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
+interface Credencial {
+  email: string;
+  password: string;
+}
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LoginService {
+  private readonly baseUrl = environment.baseApiUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  fazerLogin({ email, password }: Credencial) {
+    return this.http.post(`${this.baseUrl}/libraries/sessions`, {
+      email,
+      password,
+    });
+  }
 }
