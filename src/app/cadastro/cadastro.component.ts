018273f9e124c5f8b6eafad56fd73f698a17c16a
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-05-28 07:48:24 -0300

    Criação Layout de cadastro biblioteca

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { LoginService } from './../login/login.service';
 
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html',
-  styleUrls: ['./cadastro.component.scss']
+  styleUrls: ['./cadastro.component.scss'],
 })
 export class CadastroComponent implements OnInit {
+  formulario: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private bibliotecaService: LoginService,
+  ) {}
 
   ngOnInit(): void {
+    this.montarFormulario();
   }
 
+  private montarFormulario() {
+    this.formulario = this.formBuilder.group({
+      name: ['Angular Ngx Books', [Validators.required]],
+      email: ['angular@ngxbooks.com', [Validators.required, Validators.email]],
+      password: ['123456', [Validators.required]],
+      passwordConfirmation: ['123456', [Validators.required]],
+    });
+  }
+
+  onSubmit() {
+    this.bibliotecaService.fazerLogin(this.formulario.value).subscribe(
+      (response) => {
+        const nomeChave = '@ngx-books:biblioteca';
+        const dadosLoginBiblioteca = JSON.stringify(response);
+        localStorage.setItem(nomeChave, dadosLoginBiblioteca);
+      },
+      (error) => {},
+    );
+  }
 }
