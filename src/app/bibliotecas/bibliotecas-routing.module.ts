6c662b338ca2a2605e8b106e524e5a4089e95ae4
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-05-28 08:27:21 -0300

    Config Routa para Biblioteca e Lazy Loading

--- src/app/bibliotecas/bibliotecas-routing.module.ts
+++ src/app/bibliotecas/bibliotecas-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { BibliotecasComponent } from './bibliotecas.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: BibliotecasComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class BibliotecasRoutingModule { }
+export class BibliotecasRoutingModule {}
