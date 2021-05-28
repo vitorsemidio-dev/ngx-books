039ea116f51694555a5f3f7db44d939f633f114d
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-05-28 14:13:35 -0300

    Criado Card Item Component

--- src/app/bibliotecas/bibliotecas.module.ts
+++ src/app/bibliotecas/bibliotecas.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { BibliotecasRoutingModule } from './bibliotecas-routing.module';
 import { BibliotecasComponent } from './bibliotecas.component';
+import { CardItemComponent } from './card-item/card-item.component';
 
 
 @NgModule({
-  declarations: [BibliotecasComponent],
+  declarations: [BibliotecasComponent, CardItemComponent],
   imports: [
     CommonModule,
     BibliotecasRoutingModule
