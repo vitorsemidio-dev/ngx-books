76f308a4202d126df71412b121a63a50e64780eb
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-05-28 14:22:16 -0300

    Estrutura html card-item

--- src/app/bibliotecas/card-item/card-item.component.ts
+++ src/app/bibliotecas/card-item/card-item.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-item',
   templateUrl: './card-item.component.html',
-  styleUrls: ['./card-item.component.scss']
+  styleUrls: ['./card-item.component.scss'],
 })
 export class CardItemComponent implements OnInit {
+  @Input() imgUrl =
+    'https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=250&q=40';
 
-  constructor() { }
+  @Input() titulo = 'Default';
+  @Input() subtitulo = 'Default';
+  @Input() textoBotao = 'Default';
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
