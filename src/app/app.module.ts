ce1bbb5e3d40d29d9bfb49d36937363ef2ffd17a
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-05-28 07:11:46 -0300

    Importação HttpClientModule ao AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,7 +10,12 @@ import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, LoginComponent],
-  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
