c3c25e312acb6de4909e26100d41177d6f060e36
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-05-28 08:41:25 -0300

    Iniciado css para listagem sem dados

--- src/app/bibliotecas/bibliotecas.component.ts
+++ src/app/bibliotecas/bibliotecas.component.ts
@@ -3,13 +3,33 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-bibliotecas',
   templateUrl: './bibliotecas.component.html',
-  styleUrls: ['./bibliotecas.component.scss']
+  styleUrls: ['./bibliotecas.component.scss'],
 })
 export class BibliotecasComponent implements OnInit {
+  listagemBibliotecas: string[] = [
+    'Biblioteca 1',
+    'Biblioteca 2',
+    'Biblioteca 3',
+    'Biblioteca 4',
+    'Biblioteca 5',
+    'Biblioteca 6',
+    'Biblioteca 7',
+    'Biblioteca 8',
+    'Biblioteca 9',
+    'Biblioteca 10',
+    'Biblioteca 11',
+    'Biblioteca 12',
+    'Biblioteca 13',
+    'Biblioteca 14',
+    'Biblioteca 15',
+    'Biblioteca 16',
+    'Biblioteca 17',
+    'Biblioteca 18',
+    'Biblioteca 19',
+    'Biblioteca 20',
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
