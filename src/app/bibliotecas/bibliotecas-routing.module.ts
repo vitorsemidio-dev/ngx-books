a81d02918e70c75917568d17d069859d336f71ce
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-05-28 15:45:44 -0300

    Adicionado configuração de rota ao detalhe biblioteca

--- src/app/bibliotecas/bibliotecas-routing.module.ts
+++ src/app/bibliotecas/bibliotecas-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { BibliotecaDetalheComponent } from './biblioteca-detalhe/biblioteca-detalhe.component';
 import { BibliotecasComponent } from './bibliotecas.component';
 
-const routes: Routes = [{ path: '', component: BibliotecasComponent }];
+const routes: Routes = [
+  { path: '', component: BibliotecasComponent },
+  { path: ':slug', component: BibliotecaDetalheComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
