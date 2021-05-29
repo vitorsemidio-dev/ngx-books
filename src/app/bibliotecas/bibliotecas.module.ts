3450c25d1d0fbee39d7e23b19a880f8dbd5a02fe
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-05-29 02:30:01 -0300

    Substituição do card-item de biblioteca pelo de shared

--- src/app/bibliotecas/bibliotecas.module.ts
+++ src/app/bibliotecas/bibliotecas.module.ts
@@ -3,16 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { BibliotecasRoutingModule } from './bibliotecas-routing.module';
 import { BibliotecasComponent } from './bibliotecas.component';
-import { CardItemComponent } from './card-item/card-item.component';
 import { BibliotecaDetalheComponent } from './biblioteca-detalhe/biblioteca-detalhe.component';
 import { CardItemBibliotecaComponent } from './card-item-biblioteca/card-item-biblioteca.component';
-
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [BibliotecasComponent, CardItemComponent, BibliotecaDetalheComponent, CardItemBibliotecaComponent],
-  imports: [
-    CommonModule,
-    BibliotecasRoutingModule
-  ]
+  declarations: [
+    BibliotecasComponent,
+    BibliotecaDetalheComponent,
+    CardItemBibliotecaComponent,
+  ],
+  imports: [CommonModule, BibliotecasRoutingModule, SharedModule],
 })
-export class BibliotecasModule { }
+export class BibliotecasModule {}
