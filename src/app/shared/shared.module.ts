87d4f784156d6c2e5543b47aa3d548755d19e3e3
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-05-29 02:08:34 -0300

    Criação card-item vazio no shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CardItemComponent } from './card-item/card-item.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [CardItemComponent],
   imports: [CommonModule],
 })
 export class SharedModule {}
