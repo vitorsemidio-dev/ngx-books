d2c19c56edc4f0013052e675b80a89faeb461839
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-05-29 17:40:32 -0300

    Preenchimento de dados do perfil logado

--- src/app/bibliotecas/perfil/perfil.component.ts
+++ src/app/bibliotecas/perfil/perfil.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Biblioteca } from '../biblioteca.model';
+
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.component.html',
-  styleUrls: ['./perfil.component.scss']
+  styleUrls: ['./perfil.component.scss'],
 })
 export class PerfilComponent implements OnInit {
+  biblioteca: Biblioteca;
+  imgUrlDefault = 'https://via.placeholder.com/150';
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.carregarTela();
   }
 
+  carregarTela() {
+    this.carregarDadosPerfil();
+  }
+
+  private carregarDadosPerfil() {
+    const dadosPerfil = JSON.parse(
+      localStorage.getItem('@ngx-books:biblioteca'),
+    ) as {
+      library: Biblioteca;
+    };
+
+    this.biblioteca = dadosPerfil.library;
+  }
 }
