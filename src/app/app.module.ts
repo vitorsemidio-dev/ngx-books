24b62c368b80f7f75ee04a2b9fbdc4cc541e4daf
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-05-29 02:01:31 -0300

    Criaçao componente livros vazio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { LoginComponent } from './login/login.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
+import { LivrosComponent } from './livros/livros.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, LoginComponent, CadastroComponent],
+  declarations: [AppComponent, HeaderComponent, LoginComponent, CadastroComponent, LivrosComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
