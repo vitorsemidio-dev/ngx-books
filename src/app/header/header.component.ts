53ef1bba8b02d3ba071047415e201fb6ec3aa01e
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-05-29 22:57:12 -0300

    Adição link para perfil caso usuário logado

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Biblioteca } from '../bibliotecas/biblioteca.model';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  usuarioLogado: Biblioteca;
+  imgUrlDefault = 'https://via.placeholder.com/150';
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.carregarDadosUsuarioAutenticado();
   }
 
+  carregarDadosUsuarioAutenticado() {
+    const { library } = JSON.parse(
+      localStorage.getItem('@ngx-books:biblioteca'),
+    ) as {
+      library: Biblioteca;
+      token: string;
+    };
+
+    this.usuarioLogado = library;
+  }
 }
