69474e593f825609b10df6597bc45dd12e6505af
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-05-29 21:50:24 -0300

    Carregando detalhes do livro selecionado

--- src/app/bibliotecas/perfil-detalhe/perfil-detalhe.component.ts
+++ src/app/bibliotecas/perfil-detalhe/perfil-detalhe.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Livro } from 'src/app/livros/livro.model';
+import { LivrosService } from 'src/app/livros/livros.service';
 
 @Component({
   selector: 'app-perfil-detalhe',
   templateUrl: './perfil-detalhe.component.html',
-  styleUrls: ['./perfil-detalhe.component.scss']
+  styleUrls: ['./perfil-detalhe.component.scss'],
 })
 export class PerfilDetalheComponent implements OnInit {
+  imgUrlDefault = 'https://via.placeholder.com/150';
+  slug: string;
 
-  constructor() { }
+  livro: Livro;
+
+  constructor(
+    private livrosService: LivrosService,
+    private router: ActivatedRoute,
+  ) {}
 
   ngOnInit(): void {
+    this.carregarTela();
   }
 
+  carregarTela() {
+    this.router.params.subscribe((params) => {
+      this.slug = params['slug'];
+      this.carregarDadosLivro();
+    });
+  }
+
+  private carregarDadosLivro() {
+    this.livrosService.listar().subscribe((response) => {
+      const responseLivro = response.find((item) => item.slug === this.slug);
+      this.livro = responseLivro;
+    });
+  }
 }
