778f4f3e8b4a33c62c46bdb2fea9c11c4f5c9a6a
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-06-01 02:01:13 -0300

    movido detalhe e formulario para módulo de livro

--- src/app/livros/livros.module.ts
+++ src/app/livros/livros.module.ts
@@ -4,11 +4,24 @@ import { CommonModule } from '@angular/common';
 import { LivrosComponent } from './livros.component';
 import { CardItemLivroComponent } from './card-item-livro/card-item-livro.component';
 
+import { LivroDetalheComponent } from './livro-detalhe/livro-detalhe.component';
+import { LivroFormularioComponent } from './livro-formulario/livro-formulario.component';
+
 import { LivrosRoutingModule } from './livros-routing.module';
 
 @NgModule({
-  declarations: [LivrosComponent, CardItemLivroComponent],
+  declarations: [
+    LivrosComponent,
+    CardItemLivroComponent,
+    LivroDetalheComponent,
+    LivroFormularioComponent,
+  ],
   imports: [CommonModule, LivrosRoutingModule],
-  exports: [LivrosComponent, CardItemLivroComponent],
+  exports: [
+    LivrosComponent,
+    CardItemLivroComponent,
+    LivroDetalheComponent,
+    LivroFormularioComponent,
+  ],
 })
 export class LivrosModule {}
