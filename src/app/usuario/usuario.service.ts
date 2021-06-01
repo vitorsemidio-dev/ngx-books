71215449c45297c1ae233515d01c14ebe8e9b6af
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-06-01 18:44:59 -0300

    Serviço com endpoint para listagem de livros alugados

--- src/app/usuario/usuario.service.ts
+++ src/app/usuario/usuario.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class UsuarioService {
+import { Usuario } from './usuario';
 
-  constructor() { }
+import { CrudService } from '../shared/services/crud.service';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UsuarioService extends CrudService<Usuario> {
+  constructor(protected http: HttpClient) {
+    super(http, 'users');
+  }
+
+  listarLivrosAlugados(userId: string) {
+    return this.http.get(
+      `${this.apiUrl}/${this.recurso}/${userId}/books-rented`,
+    );
+  }
 }
