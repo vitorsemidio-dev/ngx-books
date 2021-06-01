1fb7c32bca842ad9df37e50144b2907225579f2b
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-06-01 01:59:13 -0300

    Movido Livros e CardItem components para seu módulo

--- src/app/livros/livros.module.ts
+++ src/app/livros/livros.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { LivrosComponent } from './livros.component';
+import { CardItemLivroComponent } from './card-item-livro/card-item-livro.component';
+
 import { LivrosRoutingModule } from './livros-routing.module';
 
-
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    LivrosRoutingModule
-  ]
+  declarations: [LivrosComponent, CardItemLivroComponent],
+  imports: [CommonModule, LivrosRoutingModule],
+  exports: [LivrosComponent, CardItemLivroComponent],
 })
-export class LivrosModule { }
+export class LivrosModule {}
