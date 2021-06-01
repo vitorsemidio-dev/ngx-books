cbd41bd92ae0ee6ce224bbbd4a96a2d652acf291
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-06-01 01:56:35 -0300

    Criação módulo de livros

---
+++ src/app/livros/livros.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LivrosRoutingModule } from './livros-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    LivrosRoutingModule
+  ]
+})
+export class LivrosModule { }
