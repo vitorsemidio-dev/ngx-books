1369298e069fd595d963c1281c9b4520e7c5de3f
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-05-31 22:25:12 -0300

    Criação auth service

---
+++ src/app/shared/services/auth.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Biblioteca } from 'src/app/bibliotecas/biblioteca.model';
+import { Chave } from '../chave';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor() {}
+
+  buscarInformacaoUsuarioLogado() {
+    const dadosLocalStorage = localStorage.getItem(Chave.chaveUsuarioLogado);
+
+    if (!dadosLocalStorage) {
+      return null;
+    }
+
+    const { library, token } = JSON.parse(dadosLocalStorage) as {
+      library: Biblioteca;
+      token: string;
+    };
+
+    return {
+      library,
+      token,
+    };
+  }
+}
