98fd44ee250f534a1b765712d4c37b2abe4774ae
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-06-01 02:36:21 -0300

    Movido livro resolve para seu módulo

--- src/app/livros/livros-routing.module.ts
+++ src/app/livros/livros-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { LivroResolver } from '../bibliotecas/guards/livro.resolver';
+import { LivroResolver } from './guards/livro.resolver';
 import { LivroDetalheComponent } from './livro-detalhe/livro-detalhe.component';
 import { LivroFormularioComponent } from './livro-formulario/livro-formulario.component';
 
@@ -24,29 +24,6 @@ const routes: Routes = [
       livro: LivroResolver,
     },
   },
-  // {
-  //   path: '',
-  //   children: [
-  //     {
-  //       path: 'criar-livro',
-  //       component: LivroFormularioComponent,
-  //       resolve: {
-  //         livro: LivroResolver,
-  //       },
-  //     },
-  //     {
-  //       path: ':slug',
-  //       component: LivroDetalheComponent,
-  //     },
-  //     {
-  //       path: ':slug/editar',
-  //       component: LivroFormularioComponent,
-  //       resolve: {
-  //         livro: LivroResolver,
-  //       },
-  //     },
-  //   ],
-  // },
 ];
 
 @NgModule({
