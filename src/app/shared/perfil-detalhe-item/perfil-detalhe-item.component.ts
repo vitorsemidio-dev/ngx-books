871b2fa73f91b45944a432ba1fc2245d49e52c6d
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-06-01 04:11:55 -0300

    Impelementação componente compartilhado

--- src/app/shared/perfil-detalhe-item/perfil-detalhe-item.component.ts
+++ src/app/shared/perfil-detalhe-item/perfil-detalhe-item.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-perfil-detalhe-item',
   templateUrl: './perfil-detalhe-item.component.html',
-  styleUrls: ['./perfil-detalhe-item.component.scss']
+  styleUrls: ['./perfil-detalhe-item.component.scss'],
 })
 export class PerfilDetalheItemComponent implements OnInit {
+  @Input() imgUrl: string;
+  @Input() imgDescricao: string;
+  @Input() name: string;
+  @Input() email: string;
 
-  constructor() { }
+  @Output() onSair = new EventEmitter();
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
+
+  handleSair() {}
 }
