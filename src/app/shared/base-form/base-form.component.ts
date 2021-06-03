c86024f76875c87cc050c48963e333d400b17a95
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-06-03 01:22:50 -0300

    AplicarClasseCssFeedback

--- src/app/shared/base-form/base-form.component.ts
+++ src/app/shared/base-form/base-form.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormArray, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-base-form',
   template: '<div></div>',
 })
-export abstract class BaseFormComponent implements OnInit {
+export abstract class BaseFormComponent {
   formulario: FormGroup;
   constructor() {}
-
-  ngOnInit(): void {}
 
   abstract submit(): void;
 
@@ -34,5 +32,18 @@ export abstract class BaseFormComponent implements OnInit {
 
   limparCampos() {
     this.formulario.reset();
+  }
+
+  aplicarClasseCssFeedback(nomeCampo: string) {
+    const controle = this.formulario.get(nomeCampo);
+
+    if (!controle || controle.status === 'PENDING') {
+      return {};
+    }
+
+    return {
+      'is-valid': (controle.touched || controle.dirty) && controle.valid,
+      'is-invalid': (controle.touched || controle.dirty) && !controle.valid,
+    };
   }
 }
