388ad73da3f11a2981df664f8b1c60c653dc1a51
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-06-03 00:27:35 -0300

    Criado input-field-component

--- src/app/shared/input-field/input-field.component.ts
+++ src/app/shared/input-field/input-field.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { AbstractControl, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-input-field',
   templateUrl: './input-field.component.html',
-  styleUrls: ['./input-field.component.scss']
+  styleUrls: ['./input-field.component.scss'],
 })
 export class InputFieldComponent implements OnInit {
+  @Input() classeCss: any;
+  @Input() id: string;
+  @Input() label: string;
+  @Input() type = 'text';
+  @Input() placeholder: string;
+  @Input() control: AbstractControl | FormControl;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
