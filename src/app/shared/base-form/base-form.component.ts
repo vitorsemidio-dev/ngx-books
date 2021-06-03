acd9f6641553335223238276c4f66166255ab825
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-06-03 01:09:18 -0300

    Criação base form

---
+++ src/app/shared/base-form/base-form.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-base-form',
+  template: '<div></div>',
+})
+export abstract class BaseFormComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
