a467e919da0171f58d9b994504c995fa66eb2e2a
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-06-03 01:16:04 -0300

    onSubmit, submit, validações e limpar campos

--- src/app/shared/base-form/base-form.component.ts
+++ src/app/shared/base-form/base-form.component.ts
@@ -1,11 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-base-form',
   template: '<div></div>',
 })
 export abstract class BaseFormComponent implements OnInit {
+  formulario: FormGroup;
   constructor() {}
 
   ngOnInit(): void {}
+
+  abstract submit(): void;
+
+  onSubmit() {
+    if (this.formulario.valid) {
+      this.submit();
+    } else {
+      this.verificarValidacoesFormulario(this.formulario);
+    }
+  }
+
+  verificarValidacoesFormulario(formGroup: FormGroup | FormArray) {
+    Object.keys(formGroup.controls).forEach((campo) => {
+      const control = formGroup.get(campo);
+
+      this.formulario.get(campo).markAsTouched();
+      if (control instanceof FormGroup || control instanceof FormArray) {
+        this.verificarValidacoesFormulario(control);
+      }
+    });
+  }
+
+  limparCampos() {
+    this.formulario.reset();
+  }
 }
