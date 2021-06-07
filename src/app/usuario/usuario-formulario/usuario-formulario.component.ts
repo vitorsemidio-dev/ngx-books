ef4b40aa8818c33bd9b43c8d45dda3c6600ec036
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-06-07 01:03:00 -0300

    Criado formulario usuario

---
+++ src/app/usuario/usuario-formulario/usuario-formulario.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-usuario-formulario',
+  templateUrl: './usuario-formulario.component.html',
+  styleUrls: ['./usuario-formulario.component.scss']
+})
+export class UsuarioFormularioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
