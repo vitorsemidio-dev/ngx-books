6d768b2b13178f8b263da1a692de6035791ecf76
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-06-09 20:49:20 -0300

    Implementado usuario-auth-guard

--- src/app/usuario/guards/usuario-auth.guard.ts
+++ src/app/usuario/guards/usuario-auth.guard.ts
@@ -3,17 +3,36 @@ import {
   CanActivate,
   ActivatedRouteSnapshot,
   RouterStateSnapshot,
+  Router,
 } from '@angular/router';
+
+import { RotasUrlApp } from 'src/app/shared/rotas-url-app';
+import { AlertaModalService } from 'src/app/shared/services/alerta-modal.service';
+import { AuthService } from 'src/app/shared/services/auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UsuarioAuthGuard implements CanActivate {
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private alertaModalservice: AlertaModalService,
+  ) {}
+
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot,
   ): boolean {
-    console.log('Usuario Auth');
+    const usuarioLogado = this.authService.buscarDadosUsuario();
+
+    if (!usuarioLogado) {
+      this.router.navigate([RotasUrlApp.loginUsuario]);
+      this.alertaModalservice.mostrarAlertaSucesso(
+        'Para acessar o seu perfil, faça o login primeiro',
+      );
+      return false;
+    }
     return true;
   }
 }
