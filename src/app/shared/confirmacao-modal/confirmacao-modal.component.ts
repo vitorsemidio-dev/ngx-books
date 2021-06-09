1a6c4b2495f758e43d90b187163e4c443f4b51e6
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-06-08 22:01:33 -0300

    Implementado confirmacao-modal

--- src/app/shared/confirmacao-modal/confirmacao-modal.component.ts
+++ src/app/shared/confirmacao-modal/confirmacao-modal.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { BsModalRef } from 'ngx-bootstrap/modal';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-confirmacao-modal',
   templateUrl: './confirmacao-modal.component.html',
-  styleUrls: ['./confirmacao-modal.component.scss']
+  styleUrls: ['./confirmacao-modal.component.scss'],
 })
 export class ConfirmacaoModalComponent implements OnInit {
+  @Input() titulo: string;
+  @Input() msg: string;
 
-  constructor() { }
+  @Input() btnTxtConfirmar = 'Confirmar';
+  @Input() btnTxtCancelar = 'Cancelar';
+
+  acaoConfirmacao: Subject<boolean>;
+
+  constructor(public bsModalRef: BsModalRef) {}
 
   ngOnInit(): void {
+    this.acaoConfirmacao = new Subject();
   }
 
+  onCancelar() {
+    this.confirmAndClose(false);
+  }
+
+  onConfirmar() {
+    this.confirmAndClose(true);
+  }
+
+  private confirmAndClose(valor: boolean) {
+    this.acaoConfirmacao.next(valor);
+    this.bsModalRef.hide();
+  }
 }
