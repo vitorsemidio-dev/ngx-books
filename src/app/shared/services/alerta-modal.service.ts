aadf8cfbd29e469be28e1d789d13a4491c09e811
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-06-08 22:09:12 -0300

    método para mostrar alerta confirmacao criado

--- src/app/shared/services/alerta-modal.service.ts
+++ src/app/shared/services/alerta-modal.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { BsModalService } from 'ngx-bootstrap/modal';
+
+import { ConfirmacaoModalComponent } from '../confirmacao-modal/confirmacao-modal.component';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AlertaModalService {
+  constructor(private modalService: BsModalService) {}
 
-  constructor() { }
+  mostrarAlertaConfirmacao(
+    titulo: string,
+    msg: string,
+    btnTxtConfirmar?: string,
+    btnTxtCancelar?: string,
+  ) {
+    const bsModalRef = this.modalService.show(ConfirmacaoModalComponent);
+
+    bsModalRef.content.titulo = titulo;
+    bsModalRef.content.msg = msg;
+
+    if (btnTxtConfirmar) {
+      bsModalRef.content.btnTxtConfirmar = btnTxtConfirmar;
+    }
+
+    if (btnTxtCancelar) {
+      bsModalRef.content.btnTxtCancelar = btnTxtCancelar;
+    }
+
+    return bsModalRef.content.acaoConfirmacao;
+  }
 }
